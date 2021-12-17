864c369fe27cda81300c742a094e7961736abb55
GMU-YEL <93787291+GMU-YEL@users.noreply.github.com>
2021-12-16 22:51:18 -0500

    Add files via upload

--- move-preprocessed-yel.sh
+++ move-preprocessed-yel.sh
@@ -11,13 +11,15 @@ for sub in "${!sublist[@]}";
     echo "fmriprep"
     cp -R /scratch/csng/YEL/data/output/freesurfer/sub-${sublist[sub]} /groups/YEL/PAIT2_preprocessed/freesurfer/
     echo "freesurfer"
+    cp -R /scratch/csng/YEL/data/nifti/sub-${sublist[sub]} /groups/YEL/PAIT2_preprocessed/nifti/
+    echo "nifti"
     #cp -R /scratch/csng/YEL/data/output/ciftify/sub-${sublist[sub]} /groups/YEL/PAIT2_preprocessed/ciftify/
 	cp -R /scratch/csng/YEL/data/output/stats/sub-${sublist[sub]} /groups/YEL/PAIT2_preprocessed/stats/
 	echo "stats"
     cp -R /scratch/csng/YEL/data/TextFiles/${sublist[sub]} /groups/YEL/PAIT2_preprocessed/TextFiles/ 
     echo "TextFiles"
-    cp -rf /scratch/csng/YEL/data/output/fmriprep/sub-${sublist[sub]}/func/*confounds_reduced.tsv /groups/YEL/PAIT2_preprocessed/fmriprep/sub-${sublist[sub]}/func/
-    echo "confounds_reduced"   
+    #cp -rf /scratch/csng/YEL/data/output/fmriprep/sub-${sublist[sub]}/func/*confounds_reduced.tsv /groups/YEL/PAIT2_preprocessed/fmriprep/sub-${sublist[sub]}/func/
+    #echo "confounds_reduced"   
     done
 
 
